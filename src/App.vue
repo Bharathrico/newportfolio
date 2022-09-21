f8e0ab47bb3ccbca14e19c214233f119bf06fe19
bharathrico <bharathganesh26899@gmail.com>
2022-09-21 11:10:23 +0530

    routeradd

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,30 @@
 <template>
-  <HelloWorld/>
-  <!-- <Projectpage/> -->
-  <!-- <OtherWorks/> -->
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-// import Projectpage from './components/ProjectPage.vue'
-// import OtherWorks from './components/OtherWorks.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    // Projectpage
-    // OtherWorks,
-}
-}
-</script>
-
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-body
-{
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin: 0;
-  background-color: rgb(17, 17, 17);
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: column;
-}
 #app {
-  font-family: 'Montserrat', sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
